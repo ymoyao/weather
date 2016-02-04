801529152c5e11b5bf42751144464a0913b98b36
developer_yh@163.com <developer_yh@163.com>
2016-02-04 17:00:28 +0800

    Initial Commit

---
+++ Weathers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weathers
+//
+//  Created by SR on 16/2/4.
+//  Copyright © 2016年 游辉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
